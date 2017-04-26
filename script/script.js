7ab66a22d2fb3e1ec39ec0374fdf94a0727a3e11
cherevan23 <cherevan93evgeniy@gmail.com>
2017-04-26 20:51:51 +0300

    complete footer, responsive, validation form test.

--- script/script.js
+++ script/script.js
@@ -4,24 +4,47 @@ var translateWidth = 0;
 
 $(document).ready(function () {
 
-    $('#next-btn').click(function() {
+    $('#next-btn').click(function () {
         nextSlide();
     });
 
-    $('#prev-btn').click(function() {
+    $('#prev-btn').click(function () {
         prevSlide();
     });
 
     $('.price-item__button').hover(
-        function(){
-            $(this).addClass('active_button');
-            $(this).siblings('.price-item__header').addClass('active_header');
+        function () {
+            $(this).addClass('active');
+            $(this).siblings('.price-item__header').addClass('active');
         },
-        function(){
-            $(this).removeClass('active_button');
-            $(this).siblings('.price-item__header').removeClass('active_header');
+        function () {
+            $(this).removeClass('active');
+            $(this).siblings('.price-item__header').removeClass('active');
         }
     );
+
+    $('#email').on('input propertychange', function () {
+        if ($(this).val() != '') {
+            var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
+            if (pattern.test($(this).val())) {
+                $('#email-warning').hide();
+            } else {
+                $('#email-warning').show();
+            }
+        }
+    });
+
+    $('#menu-btn').on('click', function () {
+        $('#header-menu').toggle(500 , function() {
+            // Animation complete.
+        });
+        if ($('.slider').css('margin-top') == '50px') {
+            $('.slider').animate({marginTop:350},500);
+        } else {
+            $('.slider').animate({marginTop:50},500);
+        }
+
+    });
 });
 
 function nextSlide() {
