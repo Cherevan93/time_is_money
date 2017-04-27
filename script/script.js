7c3a1df2272df2897e60dbf21a15b47bdf1e3669
cherevan23 <cherevan93evgeniy@gmail.com>
2017-04-27 14:51:30 +0300

    debug anchors, closing the drop-down menu by clicking outside, html validation

--- script/script.js
+++ script/script.js
@@ -12,6 +12,7 @@ $(document).ready(function () {
         prevSlide();
     });
 
+    //подсвечивание заголовка блока и кнопки "Buy now" по наведению ТОЛЬКО лишь на кнопку buy now.
     $('.price-item__button').hover(
         function () {
             $(this).addClass('active');
@@ -23,6 +24,7 @@ $(document).ready(function () {
         }
     );
 
+    //валидация Емаила с всплывающим окном
     $('#email').on('input propertychange', function () {
         if ($(this).val() != '') {
             var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
@@ -34,19 +36,44 @@ $(document).ready(function () {
         }
     });
 
+    //выпадающее меню
     $('#menu-btn').on('click', function () {
-        $('#header-menu').toggle(500 , function() {
-            // Animation complete.
+        $('#header-menu').toggle(500, function () {
         });
         if ($('.slider').css('margin-top') == '50px') {
-            $('.slider').animate({marginTop:350},500);
+            $('.slider').animate({marginTop: 350}, 500);
         } else {
-            $('.slider').animate({marginTop:50},500);
+            $('.slider').animate({marginTop: 50}, 500);
         }
-
     });
+    $(document).mouseup(function (e) {
+        var div = $("#header-menu");
+        var h = $("#menu-btn");
+        if (!div.is(e.target) && div.has(e.target).length === 0) {
+            if (!h.is(e.target) && h.has(e.target).length === 0) {
+                div.hide();
+                $('.slider').animate({marginTop: 50}, 500);
+            }
+        }
+    });
+
+    //правильный переход по якорям с учетом высоты фиксированного меню
+    $("#header-menu").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'), top;
+
+        if ($(window).width() <= 620) {
+            top = $(id).offset().top - 50;
+        } else {
+            top = $(id).offset().top - 100;
+        }
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+
+
 });
 
+//респонсивный слайдер
 function nextSlide() {
     if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
         $('#slide-wrapper').css('transform', 'translate(0, 0)');
